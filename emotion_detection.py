8a5855643fcaada2beb956688cbed5de6b01e87f
Cedrick Ngeja <wajaja@users.noreply.github.com>
2025-04-15 14:40:02 +0100

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,22 +2,13 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
-    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyze}}
     
-    headers = {
-        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock",
-        "Content-Type": "application/json"
-    }
-    
-    payload = {
-        "raw_document": {
-            "text": text_to_analyse
-        }
-    }
-    
-    response = requests.post(url, headers=headers, json=payload)
+    response = requests.post(url, json=input_json, headers=headers)
     
     if response.status_code == 200:
-        return response.json()
+        return response.json().emotionPredictions
     else:
-        return {"error": f"Request failed with status {response.status_code}", "details": response.text}
+        return {"error": f"Request failed with status code {response.status_code}"}
