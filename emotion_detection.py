d8e4015f23864e92af8548cca527296a5ce2adc7
Cedrick Ngeja <wajaja@users.noreply.github.com>
2025-04-15 16:04:19 +0100

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,8 +7,26 @@ def emotion_detector(text_to_analyze):
     input_json = {"raw_document": {"text": text_to_analyze}}
     
     response = requests.post(url, json=input_json, headers=headers)
-    
+    data = response.json()
+    emotions = data['emotionPredictions'][0]['emotion']
+        
+    anger_score = emotions.get('anger', 0)
+    disgust_score = emotions.get('disgust', 0)
+    fear_score = emotions.get('fear', 0)
+    joy_score = emotions.get('joy', 0)
+    sadness_score = emotions.get('sadness', 0)
+    dominant = max(emotions.items(), key=lambda x: x[1])[0]
+
     if response.status_code == 200:
-        return response.json().emotionPredictions
+        return {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant
+        }
     else:
         return {"error": f"Request failed with status code {response.status_code}"}
+
+
