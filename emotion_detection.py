0eed8954c8664ddb50f1b3acb234e2883e68ee8d
Cedrick Ngeja <wajaja@users.noreply.github.com>
2025-04-15 10:25:49 +0100

    Create emotion_detection.py

---
+++ emotion_detection.py
@@ -0,0 +1,23 @@
+
+
+
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock",
+        "Content-Type": "application/json"
+    }
+    
+    payload = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+    
+    response = requests.post(url, headers=headers, json=payload)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": f"Request failed with status {response.status_code}", "details": response.text}
