b72f7e06849f24cd4320cff25a75a1697ef3cb35
Cedrick Ngeja <wajaja@users.noreply.github.com>
2025-04-19 00:07:59 +0100

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 import requests
 
 app = Flask(__name__)
-
+//
 WATSON_URL = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
 HEADERS = {
     "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock",
